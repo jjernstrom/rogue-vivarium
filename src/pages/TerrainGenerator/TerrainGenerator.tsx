e09dfd304ef7e3b100aeb786e78562a694338972
Jeffrey Jernstrom <jeffrey.jernstrom@gmail.com>
2024-10-09 22:28:09 -0400

    Port over terrain generator from old repo

--- src/pages/TerrainGenerator/TerrainGenerator.tsx
+++ src/pages/TerrainGenerator/TerrainGenerator.tsx
@@ -1,5 +1,45 @@
+import { Box, Stack } from '@mui/material';
+import { Stage } from '@pixi/react';
+import { Vector } from './types';
+import { Terrain } from './Terrain';
+import { useState } from 'react';
+import { ControlPanel } from './Controls';
+
+const stageSize: Vector = {x: 1200, y:600};
+
 export const TerrainGenerator = () => {
-  return (
-    <div>Test</div>
+  
+const [generate, setGenerate] = useState<boolean>(false);
+const [frequency, setFrequency] = useState<number>(0.1);
+const [tileSize, setTileSize] = useState<number>(5);
+const [mapSize, setMapSize] = useState<Vector>({x:300, y:600})
+const [waterLevel, setWaterLevel] = useState<number>(25);
+ 
+  
+return (
+    <Box>
+    <div>Terrain Generator</div>
+    <Stack>
+      <Stage width={stageSize.x} height={stageSize.y}>
+        <Terrain 
+          generate={generate} 
+          frequency={frequency} 
+          tileSize={tileSize}
+          mapSize={mapSize}
+          waterLevel={waterLevel}
+        />
+      </Stage>
+      <ControlPanel 
+        generate={generate} 
+        setGenerate={setGenerate} 
+        frequency={frequency} 
+        setFrequency={setFrequency}
+        setMapSize={setMapSize}
+        setTileSize={setTileSize}
+        waterLevel={waterLevel}
+        setWaterLevel={setWaterLevel}
+      />
+    </Stack>
+    </Box>
   );
 }
